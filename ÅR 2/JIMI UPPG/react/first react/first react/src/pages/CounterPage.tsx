8593c091158a6e5fa1ebdfbb7632c57abcf87ce8
linahansten <skolkontolina@gmail.com>
2023-11-06 09:03:12 +0200

    L

--- ÅR 2/JIMI UPPG/react/first react/first react/src/pages/CounterPage.tsx
+++ ÅR 2/JIMI UPPG/react/first react/first react/src/pages/CounterPage.tsx
@@ -1,23 +1,8 @@
-import { useState } from "react"
-
+import Counter from "../components/Counter"
 export default function CounterPage(){
-
-   //let count = 10
-   const [count, setCount] = useState(0)
-
-   const add = () => {
-    //count = count + 1
-    setCount(count+1)
-   }
-   const minus = () => {
-    //count = count + 1
-    setCount(count-1)
-   }
     return(
     <div  className="h-screen flex justify-center items-center">
-        <button onClick={minus} className="px-4 py-2 border rounded hover:bg-cyan-100">-</button>
-        <span className="mx-4 text-4xl font-medium">{count}</span>
-        <button onClick={add} className="px-4 py-2 border rounded hover:bg-cyan-100">+</button>
+       <Counter/>
     </div>
     )
 }
